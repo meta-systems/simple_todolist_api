9b1951ed34be34f2454e0e2e885709f0cd7338fd
vitalcrazz <zojl@yandex.ru>
2019-11-21 15:53:26 +0300

    disable cors

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\TaskList;
+use yii\filters\Cors;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -13,18 +14,10 @@ class ApiController extends Controller
     public function behaviors()
     {
         return [
-            /**
-             * @see https://www.html5rocks.com/static/images/cors_server_flowchart.png
-             */
-            'corsFilter'  => [
-                'class' => \yii\filters\Cors::className(),
+            'cors'  => [
+                'class' => Cors::className(),
                 'cors'  => [
-                    'Origin'                           => ['http://localhost:8082'],
-                    'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Max-Age'           => 86400, // 24h
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Request-Headers'   => ['Authorization', 'pragma', 'cache-control', 'content-type'],
-                    'Access-Control-Expose-Headers'    => ['Authorization', 'pragma', 'cache-control', 'content-type'],
+                    'Origin' => ['*'],
                 ],
             ],
             'verbs' => [
